6c00f9cc3e01c1c9a14c6abd87870304ea9e803d
V <sthomas2357@gmail.com>
2023-01-03 14:20:48 -0800

    Start

---
+++ pong_game/main.py
@@ -0,0 +1,11 @@
+
+
+
+
+# TODO:scoarboard. Line down the middle and points on each side. Player 1 vs 2
+#
+# TODO: Play bodies. on each side. 1 vs 2
+#
+# TODO: Ball. Ball movement?
+#
+# TODO: hit and bounce back at an angle? or out of boundary.
