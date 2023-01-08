572a39b75aca49cdcb21339695e53134ab0eaa51
V <sthomas2357@gmail.com>
2023-01-08 03:33:28 -0800

    update

--- turtle-crossing-start/main.py
+++ turtle-crossing-start/main.py
@@ -12,3 +12,15 @@ game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+
+# TODO:Detect when the turtle player collides with a car and stop the game if this happens. If you get stuck,
+#  check the video walkthrough in Step 5.
+
+# TODO:Detect when the turtle player has reached the top edge of the screen (i.e., reached the FINISH_LINE_Y). When
+#  this happens, return the turtle to the starting position and increase the speed of the cars. Hint: think about
+#  creating an attribute and using the MOVE_INCREMENT to increase the car speed. If you get stuck, check the video
+#  walkthrough in Step 6.
+
+# TODO:Create a scoreboard that keeps track of which level the user is on. Every time the turtle player does a
+#  successful crossing, the level should increase. When the turtle hits a car, GAME OVER should be displayed in the
+#  centre. If you get stuck, check the video walkthrough in Step 7.
--- turtle-crossing-start/player.py
+++ turtle-crossing-start/player.py
@@ -5,3 +5,6 @@ FINISH_LINE_Y = 280
 
 class Player:
     pass
+
+# TODO: Create a turtle player that starts at the bottom of the screen and listen for the "Up"
+#  keypress to move the turtle north. If you get stuck, check the video walkthrough in Step 3.
