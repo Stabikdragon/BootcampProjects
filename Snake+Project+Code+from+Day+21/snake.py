cbcb89da81b68d4a51ac3b4d0eb415131e7e6b48
V <sthomas2357@gmail.com>
2023-01-18 21:36:06 -0800

    Update

--- Snake+Project+Code+from+Day+21/snake.py
+++ Snake+Project+Code+from+Day+21/snake.py
@@ -38,6 +38,12 @@ class Snake:
     def up(self):
         if self.head.heading() != DOWN:
             self.head.setheading(UP)
+    def reset(self):
+        for seg in self.segments:
+            seg.goto(1000, 1000)
+        self.segments.clear()
+        self.create_snake()
+        self.head = self.segments[0]
 
     def down(self):
         if self.head.heading() != UP:
